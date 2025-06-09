6d8a289ce264d1ee8a42e422677cf65a0e14d407
Simon Francisco <121758120+SimonFrancisco@users.noreply.github.com>
2025-06-09 23:27:34 +0700

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,16 +2,19 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "francisco.simon.myfinance"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "francisco.simon.myfinance"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -43,17 +46,17 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+
+    implementation(libs.bundles.compose)
+    implementation(libs.bundles.android.ui)
+    debugImplementation(libs.bundles.compose.debug)
+    androidTestImplementation(libs.bundles.android.test)
+    implementation(libs.bundles.hilt)
+    implementation(libs.bundles.serialization)
+    ksp(libs.bundles.ksp)
+
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+
 }
